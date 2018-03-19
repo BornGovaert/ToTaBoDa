922fde0910539ef615df30eaba0f333225ed1fa0
tanialemos <tanialemosribeiro@gmail.com>
2018-03-19 19:51:30 +0100

    tests BookRepository done!

--- domain/src/test/be/totaboda/Repositories/BookRepositoryTest.java
+++ domain/src/test/be/totaboda/Repositories/BookRepositoryTest.java
@@ -1,26 +1,17 @@
 package be.totaboda.Repositories;
 
+
 import be.totaboda.Book.Author;
 import be.totaboda.Book.Book;
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BookRepositoryTest {
-
-
-    List<Book> testList;
-
-    public BookRepositoryTest() {
-        testList = new ArrayList<>();
-    }
 
 
     @Test
@@ -40,8 +31,9 @@ public class BookRepositoryTest {
 
         List<Book> actualBooks = BookRepository.getBookInformationISBN("1");
 
-        Assertions.assertThat(actualBooks).contains(expectedBook1, expectedBook2);
-        Assertions.assertThat(actualBooks).doesNotContain(notExpectedBook);
+        Assertions.assertThat(actualBooks)
+                .contains(expectedBook1, expectedBook2)
+                .doesNotContain(notExpectedBook);
     }
 
     @Test
@@ -70,8 +62,9 @@ public class BookRepositoryTest {
 
         List<Book> actualBooks = BookRepository.getBookInformationISBN("n");
 
-        Assertions.assertThat(actualBooks).contains(expectedBook1, expectedBook2);
-        Assertions.assertThat(actualBooks).doesNotContain(notExpectedBook);
+        Assertions.assertThat(actualBooks)
+                .contains(expectedBook1, expectedBook2)
+                .doesNotContain(notExpectedBook);
     }
 
     @Test
@@ -84,8 +77,46 @@ public class BookRepositoryTest {
     }
 
     @Test
-    void getBookGivenPartialAuthor_happyPath(){
+    void getBooksGivenAuthor_happyPath(){
+        Author author = new Author("2", "Dan", "Brown");
+        Book expectedBook = new Book("111", "DaVinci", AuthorRepository.getAuthorDatabase().get("2"));
+        Book notExpectedBook = new Book("666", "Kaas", AuthorRepository.getAuthorDatabase().get("3"));
 
+        List<Book> actualBooks = BookRepository.getBooksGivenAuthor(author);
+
+        Assertions.assertThat(actualBooks)
+                .contains(expectedBook)
+                .doesNotContain(notExpectedBook);
     }
 
+    @Test
+    void getBooksGivenAuthor_givenNonExistingAuthor_throwIllegalArgumentException(){
+        Author author = new Author("89", "Dag", "Allemal");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                BookRepository.getBooksGivenAuthor(author));
+
+        assertEquals(exception.getMessage(), "No books found for author:");
+    }
+
+    @Test
+    void getBookGivenPartialAuthor_givenLetter_N_returnsListOfBooksWhereAuthorContainsLetter_N(){
+        Book expectedBook1 = new Book("123", "Azkaban", AuthorRepository.getAuthorDatabase().get("1"));
+        Book expectedBook2 = new Book("111", "DaVinci", AuthorRepository.getAuthorDatabase().get("2"));
+        Book notExpectedBook = new Book("666", "Kaas", AuthorRepository.getAuthorDatabase().get("3"));
+
+        List<Book> actualBooks = BookRepository.getBookGivenPartialAuthor("n");
+
+        Assertions.assertThat(actualBooks)
+                .contains(expectedBook1, expectedBook2)
+                .doesNotContain(notExpectedBook);
+    }
+
+    @Test
+    void getBookGivenPartialAuthor_givenNonExistentString_throwsIllegalArgumentExceptio(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            BookRepository.getBookGivenPartialAuthor("sdjfjkskdf");
+        });
+
+        assertEquals(exception.getMessage(), "No books found for author:sdjfjkskdf");
+    }
 }
