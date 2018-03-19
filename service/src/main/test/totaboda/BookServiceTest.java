f5bcb325634be0a54d9515c1c3db69f59987d2e0
tanialemos <tanialemosribeiro@gmail.com>
2018-03-19 14:58:49 +0100

    getBook_givenISBN_happyPath DONE

--- service/src/main/test/totaboda/BookServiceTest.java
+++ service/src/main/test/totaboda/BookServiceTest.java
@@ -1,14 +1,27 @@
 package totaboda;
 
+import be.totaboda.Book.Author;
+import be.totaboda.Book.Book;
+import be.totaboda.Repositories.BookRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class BookServiceTest {
 
-    @Test
-    void getBooks_happyPath(){
+    Book testBook = new Book("007", "James Bond", new Author("78", "Ian", "Flemming"));
 
+    @Test
+    void getBook_givenISBN_happyPath(){
+        BookService testService = new BookService();
+        BookRepository testRepo = Mockito.mock(BookRepository.class);
+
+        when(testRepo.getBookInformation("007")).thenReturn(testBook);
+
+        Assertions.assertThat(testService.getBook("007")).isEqualTo(testBook);
     }
 
 }
