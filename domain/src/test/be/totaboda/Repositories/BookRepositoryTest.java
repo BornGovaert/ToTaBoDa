9cce38dc1902d81b23cd144e651cb4ab8af0f7f0
tanialemos <tanialemosribeiro@gmail.com>
2018-03-19 16:12:54 +0100

    tests ongoing

---
+++ domain/src/test/be/totaboda/Repositories/BookRepositoryTest.java
@@ -0,0 +1,19 @@
+package be.totaboda.Repositories;
+
+import be.totaboda.Book.Author;
+import be.totaboda.Book.Book;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookRepositoryTest {
+
+//    @Test
+//    void getBookInformationISBN_happyPath(){
+//        List<Book> actualResult = BookRepository.getBookInformationISBN()
+//    }
+
+
+}
--- service/src/main/test/totaboda/BookServiceTest.java
+++ service/src/main/test/totaboda/BookServiceTest.java
@@ -4,24 +4,42 @@ import be.totaboda.Book.Author;
 import be.totaboda.Book.Book;
 import be.totaboda.Repositories.BookRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class BookServiceTest {
 
-    Book testBook = new Book("007", "James Bond", new Author("78", "Ian", "Flemming"));
-
-    @Test
-    void getBook_givenISBN_happyPath(){
-        BookService testService = new BookService();
-        BookRepository testRepo = Mockito.mock(BookRepository.class);
-
-        when(testRepo.getBookInformation("007")).thenReturn(testBook);
-
-        Assertions.assertThat(testService.getBook("007")).isEqualTo(testBook);
-    }
+//    List<Book> testList = new ArrayList<>();
+//    Book book1 = new Book("007", "James Bond", new Author("78", "Ian", "Flemming"));
+//    Book book2 = new Book("072", "James Bond 2", new Author("78", "Ian", "Flemming"));
+//    Book book3 = new Book("5232ABF", "The History of PingPong", new Author("2", "John", "Smith"));
+//
+//    @Test
+//    void getBookISBN_thenMockCallingMethod(){
+//        testList.add(book1);
+//        testList.add(book2);
+//        testList.add(book3);
+//
+//        BookService testService = new BookService();
+//        //BookRepository testRepo = Mockito.mock(BookRepository.class);
+//
+//        when(BookRepository.getBookInformationISBN("007")).thenReturn(testList);
+//
+//        Assertions.assertThat(testService.getBookISBN("007")).isEqualTo(testList);
+//       // verify(BookRepository.getBookInformationISBN("007"), times(1));
+//    }
+//
+//    @Test
+//    void getBookISBN_givenWrongISBN_thenMockIllegalArgumentException
 
 }
