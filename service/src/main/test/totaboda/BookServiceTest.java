c8277901e3ce10214f9343380643f5f51b256cd1
tanialemos <tanialemosribeiro@gmail.com>
2018-03-19 14:42:56 +0100

    Tests in progress

--- service/src/main/test/totaboda/BookServiceTest.java
+++ service/src/main/test/totaboda/BookServiceTest.java
@@ -1,7 +1,14 @@
 package totaboda;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookServiceTest {
+
+    @Test
+    void getBooks_happyPath(){
+
+    }
 
 }
