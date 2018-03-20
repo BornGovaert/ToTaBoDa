3cc309c9d0a36e5c9d9688a705ccb3dddb14c0ee
llodavid <david_vandenbergh@hotmail.com>
2018-03-20 14:20:20 +0100

    addes some mappings

--- api/src/main/java/totaboda/controller/UserController.java
+++ api/src/main/java/totaboda/controller/UserController.java
@@ -1,11 +1,35 @@
 package totaboda.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import be.totaboda.users.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import totaboda.UserService;
+import totaboda.dtos.UserDto;
+import totaboda.mapper.UserMapper;
+
+import javax.inject.Inject;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    UserService userService;
+    UserMapper userMapper;
 
+    @Inject
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
 
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto createMember (@RequestBody UserDto member) {
+        Member newMember = (Member) userService.addUser(userMapper.toMember(member));
+        return userMapper.memberToDto(newMember);
+    }
+    @GetMapping(path = "/{memberID}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto getMember(@PathVariable int memberID) {
+        return userMapper.memberToDto((Member) userService.getUser(memberID));
+    }
 }
