3b40b8fd858a7936338df1716ee3aa182cf16f3e
llodavid <david_vandenbergh@hotmail.com>
2018-03-20 13:09:14 +0100

    change to tests

--- service/src/main/test/totaboda/UserServiceTest.java
+++ service/src/main/test/totaboda/UserServiceTest.java
@@ -21,56 +21,61 @@ class UserServiceTest {
     private LoggedInUser member1;
     private LoggedInUser employee1;
 
-//    @BeforeEach
-//    public void setUp(){
-//        mockRepository = Mockito.mock(UserRepository.class);
-//        userService = new UserService(mockRepository);
-//        member1= UserBuilder.buildUser()
-//                .withFirstName("L")
-//                .withLastName("T")
-//                .withEMail("t@l.be")
-//                .withStreetNumber("4")
-//                .withStreetName("x")
-//                .withPostalCode("1")
-//                .withCity("c")
-//                .withInss("3")
-//                .buildMember();
-//
-//        employee1=UserBuilder.buildUser()
-//                .withFirstName("John")
-//                .withLastName("Constantine")
-//                .withRole(Role.ROLE_ADMIN)
-//                .withEMail("john.constantine@hell.com")
-//                .buildEmployee();
-//    }
-//
-//    @Test
-//    public void getUser_happyPath(){
-//        Mockito.when(mockRepository.getUserById(0)).thenReturn(member1);
-//        Assertions.assertThat(userService.getUser(0)).isEqualTo(member1);
-//    }
-//
-//    @Test
-//    void getUser_whenANonExistingIdIsProvided_throwsexception(){
-//        Assertions.assertThatExceptionOfType(UnknownResourceException.class)
-//                .isThrownBy(()->userService.getUser(5));
-//    }
-//
-//    @Test
-//    public void removeUser_happyPath(){
-//        UserService userService = new UserService(new UserRepository(new UserData()));
-//        int expectedSize = userService.getAllUsers().size()-1;
-//        LoggedInUser removedUser = userService.getUser(0);
-//        userService.removeUser(0);
-//        Assertions.assertThat(userService.getAllUsers().size()).isEqualTo(expectedSize);
-//        Assertions.assertThat(!userService.getAllUsers().contains(removedUser));
-//    }
-//
-//    @Test
-//    void removeUser_whenANonExistingIdIsProvided_throwsexception() {
-//        Assertions.assertThatExceptionOfType(UnknownResourceException.class)
-//                .isThrownBy(() -> userService.getUser(5));
-//    }
+    @BeforeEach
+    public void setUp(){
+        mockRepository = Mockito.mock(UserRepository.class);
+        userService = new UserService(mockRepository);
+        member1= UserBuilder.buildUser()
+                .withFirstName("L")
+                .withLastName("T")
+                .withEMail("t@l.be")
+                .withStreetNumber("4")
+                .withStreetName("x")
+                .withPostalCode("1")
+                .withCity("c")
+                .withInss("3")
+                .buildMember();
 
+        employee1=UserBuilder.buildUser()
+                .withFirstName("John")
+                .withLastName("Constantine")
+                .withRole(Role.ROLE_ADMIN)
+                .withEMail("john.constantine@hell.com")
+                .buildEmployee();
+    }
+
+    @Test
+    public void getUser_happyPath(){
+        Mockito.when(mockRepository.getUserById(0)).thenReturn(member1);
+        Assertions.assertThat(userService.getUser(0)).isEqualTo(member1);
+    }
+
+    @Test
+    void getUser_whenANonExistingIdIsProvided_throwsexception(){
+        Assertions.assertThatExceptionOfType(UnknownResourceException.class)
+                .isThrownBy(()->userService.getUser(5));
+    }
+
+    @Test
+    public void removeUser_happyPath(){
+        UserService userService = new UserService(new UserRepository(new UserData()));
+        int expectedSize = userService.getAllUsers().size()-1;
+        LoggedInUser removedUser = userService.getUser(0);
+        userService.removeUser(0);
+        Assertions.assertThat(userService.getAllUsers().size()).isEqualTo(expectedSize);
+        Assertions.assertThat(!userService.getAllUsers().contains(removedUser));
+    }
+
+    @Test
+    void removeUser_whenANonExistingIdIsProvided_throwsexception() {
+        Assertions.assertThatExceptionOfType(UnknownResourceException.class)
+                .isThrownBy(() -> userService.getUser(5));
+    }
+    @Test
+    void addUser_happyPath() {
+        Mockito.when(mockRepository.addUser(member1)).thenReturn(null);
+        Assertions.assertThat(userService.getUser(0)).isEqualTo(member1);
+
+    }
 
 }
