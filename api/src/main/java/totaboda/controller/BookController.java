6a9c1b8db48907105338458463c25dc87ab3462c
tanialemos <tanialemosribeiro@gmail.com>
2018-03-20 15:16:31 +0100

    getBooksByIsbn done!

--- api/src/main/java/totaboda/controller/BookController.java
+++ api/src/main/java/totaboda/controller/BookController.java
@@ -11,7 +11,9 @@ import totaboda.dtos.BookDto;
 import totaboda.mapper.BookMapper;
 
 import javax.inject.Inject;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -35,5 +37,14 @@ public class BookController {
         return booksDto;
     }
 
-
+    @GetMapping(path = "/{isbn}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public List<BookDto> getBookByIsbn(String isbn){
+        ArrayList<BookDto> booksByIsbn = new ArrayList<>();
+        List<Book> booksFoundMatchingIsbn = bookService.getBookISBN(isbn);
+        for(Book book : booksFoundMatchingIsbn){
+            booksByIsbn.add(BookMapper.bookMapper(book));
+        }
+        return booksByIsbn;
+    }
 }
