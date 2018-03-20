3178e975b94649f58d776265c5ee86ffae5750ab
ths-laurent <thslaurent@gmail.com>
2018-03-20 08:55:38 +0100

    added checks for illegalargumentexception in UserRepository + tests done

--- domain/src/test/be/totaboda/Repositories/UserRepositoryTest.java
+++ domain/src/test/be/totaboda/Repositories/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package be.totaboda.Repositories;
 import be.totaboda.Users.LoggedInUser;
 import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -24,7 +25,6 @@ class UserRepositoryTest {
     public void setUp() {
 
         mock = Mockito.mock(UserData.class);
-        testRepository = new UserRepository(mock);
         user1 = LoggedInUser.UserBuilder.buildUser()
                 .withFirstName("T")
                 .withLastName("L")
@@ -48,6 +48,12 @@ class UserRepositoryTest {
                 .build();
     }
 
+    @AfterEach
+
+    public void cleaning() {
+        testRepository = null;
+    }
+
     @Test
     public void getAllUsers_HappyPath() {
         Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1, user2));
@@ -56,6 +62,79 @@ class UserRepositoryTest {
 
         List<LoggedInUser> expectedUsers = testRepository.getAllUsers();
 
-        Assertions.assertThat(expectedUsers).isEqualTo(Arrays.asList(user1,user2));
+        Assertions.assertThat(expectedUsers).isEqualTo(Arrays.asList(user1, user2));
+    }
+
+    @Test
+    public void getUserWithId_happyPath() {
+        Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1, user2));
+
+        testRepository = new UserRepository(mock);
+
+        Assertions.assertThat(testRepository.getUserById(0)).isEqualTo(user1);
+
+    }
+
+    @Test
+    public void getUserWithId_whenANonExistingIDIsProvided_throwsException() {
+        Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1, user2));
+
+        testRepository = new UserRepository(mock);
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> testRepository.getUserById(3))
+                .withMessage("No such user found.");
+    }
+
+    @Test
+    public void removeUser_HappyPath() {
+        Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1, user2));
+
+        testRepository = new UserRepository(mock);
+        testRepository.removeUser(0);
+
+        Assertions.assertThat(testRepository.getAllUsers().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void removeUser_whenANonExistingIDIsProvided_throwsException() {
+        Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1, user2));
+
+        testRepository = new UserRepository(mock);
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> testRepository.removeUser(3))
+                .withMessage("No such user found.");
+    }
+
+    @Test
+    public void updateUser_HappyPath() {
+        Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1));
+
+        testRepository = new UserRepository(mock);
+        testRepository.updateUser(0, user2);
+
+        Assertions.assertThat(testRepository.getUserById(0)).isEqualTo(user2);
+    }
+
+    @Test
+    public void updateUser_whenANonExistingIDIsProvided_throwsException() {
+        Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1, user2));
+
+        testRepository = new UserRepository(mock);
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> testRepository.updateUser(3, user1))
+                .withMessage("No such user found.");
+    }
+
+    @Test
+    public void addUser_HappyPath() {
+        Mockito.when(mock.getDefaultUsers()).thenReturn(Arrays.asList(user1));
+
+        testRepository = new UserRepository(mock);
+        testRepository.addUser(user2);
+        Assertions.assertThat(testRepository.getAllUsers()).containsExactly(user1, user2);
+
     }
 }
