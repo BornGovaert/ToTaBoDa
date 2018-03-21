97cca10493978085306fc6154b9d94ac7e443de0
tanialemos <tanialemosribeiro@gmail.com>
2018-03-21 13:05:26 +0100

    UnknownUserException done

--- api/src/main/java/totaboda/controller/GlobalAdviceController.java
+++ api/src/main/java/totaboda/controller/GlobalAdviceController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import totaboda.UserService;
 import totaboda.exceptions.UnknownResourceException;
+import totaboda.exceptions.UnknownUserException;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,6 +17,15 @@ public class GlobalAdviceController {
 
     @ExceptionHandler(UnknownResourceException.class)
     public ResponseEntity<String> returnStatusForUnknownIdException(final UnknownResourceException exception) {
+        LOGGER.log(Level.SEVERE,"ERROR: " + exception.getMessage());
+        return new ResponseEntity<>(
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnknownUserException.class)
+    public ResponseEntity<String> returnSatusForUnknownUserException(final UnknownUserException exception)
+    {
         LOGGER.log(Level.SEVERE,"ERROR: " + exception.getMessage());
         return new ResponseEntity<>(
                 exception.getMessage(),
