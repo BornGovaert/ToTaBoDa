8fcdd33703a3b73d47fa4a5664db2df2dd7cb4b8
llodavid <david.vandenbergh@cm.be>
2018-03-22 13:26:00 +0100

    small changes

--- api/src/main/java/be/totaboda/api/config/WebSecurityConfiguration.java
+++ api/src/main/java/be/totaboda/api/config/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 //package totaboda.config;
-//
+
+//import javax.inject.Named;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,4 +33,16 @@
 //    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.inMemoryAuthentication().withUser("Admin").password("admin123").roles("ADMIN");
 //    }
+//    @Named
+//    @Override
+//    public UserDetailsService userDetailsService(){
+//            Role role=Enum.valueOf(Role.class,"ROLE_ADMIN");
+//            UserDetails user=
+//            User.withUsername("ADMIN")
+//            .password("password")
+//            .roles("ADMIN")
+//            .build();
+//
+//            return new InMemoryUserDetailsManager(Collections.singleton(user));
+//     }
 //}
