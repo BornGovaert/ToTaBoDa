4b09f7c678b760ef60a479518387aaf20ee71d67
BornGovaert <borngovaert@outlook.be>
2018-03-22 14:41:21 +0100

    dto and mapper rental

---
+++ api/src/main/java/be/totaboda/api/rental/RentalMapper.java
@@ -0,0 +1,19 @@
+package be.totaboda.api.rental;
+
+import be.totaboda.domain.rental.Rental;
+
+public class RentalMapper {
+
+    public static RentalDto rentalToDto(Rental rentalToMap){
+        return new RentalDto()
+                .withId(rentalToMap.getId())
+                .withBookIsbn(rentalToMap.getBookIsbn())
+                .withMemberId(rentalToMap.getMemberId())
+                .withReturnDate(rentalToMap.getReturnDate())
+                .withStartDate(rentalToMap.getStartDate());
+    }
+
+    public static Rental dtoToRental(RentalDto rental) {
+        return new Rental(rental.id, rental.bookIsbn);
+    }
+}
