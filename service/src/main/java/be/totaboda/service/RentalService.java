6b1d22d5ff46cac40430ba99325c67348be663e5
BornGovaert <borngovaert@outlook.be>
2018-03-22 13:18:39 +0100

    fixed rentalservice

--- service/src/main/java/be/totaboda/service/RentalService.java
+++ service/src/main/java/be/totaboda/service/RentalService.java
@@ -1,7 +1,11 @@
 package be.totaboda.service;
 
 
+import be.totaboda.domain.book.Book;
+import be.totaboda.domain.book.BookRepository;
 import be.totaboda.domain.rental.RentalRepository;
+import be.totaboda.domain.users.Member;
+import be.totaboda.domain.users.UserRepository;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -10,12 +14,36 @@ import java.util.logging.Logger;
 @Named
 public class RentalService {
 
-    private RentalRepository rentalRepository;
     private final static Logger LOGGER = Logger.getLogger(BookService.class.getName());
+    private RentalRepository rentalRepository;
+    private static UserRepository userRepository;
+    private BookRepository bookRepository;
 
     @Inject
-    public RentalService(RentalRepository rentalRepository){
+    public RentalService (UserRepository userRepository) {
+        RentalService.userRepository = userRepository;
+    }
+
+    @Inject
+    public RentalService(RentalRepository rentalRepository) {
         this.rentalRepository = rentalRepository;
+    }
+
+    @Inject
+    public RentalService(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    public void createRental(String memberInss, String bookIsbn) {
+        for (Member member: userRepository.getAllMembers()) {
+            if (member.getInss().equals(memberInss)) {
+                for (Book book: bookRepository.getBooks())
+                rentalRepository.createRental(memberInss, bookIsbn);
+            } else {
+                throw new IllegalArgumentException("Please provide a valid Inss");
+            }
+        }
+
     }
 
 
