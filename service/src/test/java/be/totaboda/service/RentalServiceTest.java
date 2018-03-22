aed7f2c6807d6a20ddbbb71fdca2c300b1ac4d25
tanialemos <tanialemosribeiro@gmail.com>
2018-03-22 15:58:36 +0100

    createRental_incorrectMemberAndBookId done

--- service/src/test/java/be/totaboda/service/RentalServiceTest.java
+++ service/src/test/java/be/totaboda/service/RentalServiceTest.java
@@ -1,28 +1,20 @@
 package be.totaboda.service;
 
-import be.totaboda.domain.author.Author;
-import be.totaboda.domain.author.AuthorRepository;
-import be.totaboda.domain.book.Book;
 import be.totaboda.domain.book.BookRepository;
 import be.totaboda.domain.rental.RentalRepository;
 import be.totaboda.domain.users.UserRepository;
-import org.junit.jupiter.api.BeforeAll;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class RentalServiceTest {
 
     static RentalService rentalService;
-    static AuthorRepository authorRepo;
     static RentalRepository mockRentalRepo;
     static UserRepository mockUserRepo;
     static BookRepository mockBookRepo;
-    Book book1;
-    Book book2;
-    Book book3;
 
     @BeforeEach
     void setUp(){
@@ -38,4 +30,26 @@ class RentalServiceTest {
         rentalService.createRental(1, "111");
         verify(mockRentalRepo, times (1)).createRental(1,"111");
     }
+
+    @Test
+    void createRental_givenIncorrectBookIsbn_hrowException(){
+        doThrow(new IllegalArgumentException("Please provide valid id or isbn")).when(mockRentalRepo).createRental(1, "555");
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> {
+            rentalService.createRental(1, "555");
+        });
+    }
+
+    @Test
+    void createRental_givenIncorrectMemberInss_throwException(){
+        doThrow(new IllegalArgumentException("Please provide valid id or isbn")).when(mockRentalRepo).createRental(10, "111");
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> {
+            rentalService.createRental(10, "111");
+        });
+    }
+
+
 }
