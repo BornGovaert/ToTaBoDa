b379dc48ed9b5a6cd39d41048890d4c456512879
llodavid <david.vandenbergh@cm.be>
2018-03-22 09:51:36 +0100

    big refactor

--- jar/src/main/java/be/totaboda/Digibooky.java
+++ jar/src/main/java/be/totaboda/Digibooky.java
@@ -3,7 +3,7 @@ package be.totaboda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"be/totaboda"})
+@SpringBootApplication(scanBasePackages = {"be.totaboda"})
 public class Digibooky {
     public static void main(String[] args) {
         SpringApplication.run(Digibooky.class, args);
