ae6eb94ebeea8ae31df948cdaafe21a31cc45c92
BornGovaert <borngovaert@outlook.be>
2018-03-22 16:02:02 +0100

    rental repository and rental service

--- api/src/main/java/be/totaboda/api/rental/RentalController.java
+++ api/src/main/java/be/totaboda/api/rental/RentalController.java
@@ -19,7 +19,13 @@ public class RentalController {
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public RentalDto createBook(@RequestBody RentalDto rental){
+    public RentalDto createRental(@RequestBody RentalDto rental){
         return RentalMapper.rentalToDto(rentalService.createRental(rental.memberId, rental.bookIsbn));
+    }
+
+    @DeleteMapping(path ="/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void returnBook(@PathVariable int id) {
+        rentalService.returnBook(id);
     }
 }
