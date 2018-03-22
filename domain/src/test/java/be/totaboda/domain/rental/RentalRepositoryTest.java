847d4c4707e867915d7d79a8706c51df7df407b5
tanialemos <tanialemosribeiro@gmail.com>
2018-03-22 12:54:10 +0100

    createRental_bookUnavailable() done!

--- domain/src/test/java/be/totaboda/domain/rental/RentalRepositoryTest.java
+++ domain/src/test/java/be/totaboda/domain/rental/RentalRepositoryTest.java
@@ -31,18 +31,28 @@ class RentalRepositoryTest {
 
     @Test
     void createRental_givenWrongMemberINSS_throwException() {
-
+        Assertions.fail("to be implemented");
     }
 
 
     @Test
     void createRental_givenWrongBookISBN_throwException() {
+        Assertions.fail("to be implemented");
     }
 
 
-//    @Test
-//    void createRental_givenUnavailableBook_throwException() {
-//    }
+    @Test
+    void createRental_givenUnavailableBook_throwException() {
+        rentalRepo.createRental("791101 199 53", "123");
+        rentalRepo.createRental("820101 383 03", "666");
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+        -> {
+            rentalRepo.createRental("820101 383 03", "123");
+        });
+
+        Assertions.assertThat(exception.getMessage()).isEqualTo("\"This book is already rented out\"");
+    }
 
     @Test
     void createRental_givenMaxOf9RentalsPerMember_throwException(){
